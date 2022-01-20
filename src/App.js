52a0d0105d636ad3e9d3efff57c59a20e61e4613
sabil62 <shresthasabil62@gmail.com>
2022-01-20 15:58:24 +0545

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Complete from "./calculation form/complete";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {" "}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Complete />
+    </React.Fragment>
   );
 }
 
