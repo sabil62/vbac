9535f1d49ff0fb7fa564d93682656ce832c8c871
sabil62 <shresthasabil62@gmail.com>
2022-02-23 13:45:30 +0545

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Complete from "./components/calculation form/complete";
 import LandingPage from "./components/landing page/landingPage";
 import Antenatal from "./components/calculation form/antenatal";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/UI/footer";
+import ReactGA from "react-ga";
+import { withRouter } from "./withrouter";
 
 function App() {
-  let a = true;
+  // let a = true;
+
+  useEffect(() => {
+    ReactGA.initialize("UA-154571472-2");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+
+    console.log(window.location.pathname);
+  }, []);
+
   return (
     <React.Fragment>
       <Routes>
-        <Route path="/" element={a && <LandingPage />} />
+        {/* <Route path="/" element={a && <LandingPage />} /> */}
+        <Route path="/" element={<LandingPage />} />
         <Route path="/complete" element={<Complete />} />
         <Route path="/antenatal" element={<Antenatal />} />
       </Routes>
@@ -20,4 +31,4 @@ function App() {
   );
 }
 
-export default App;
+export default withRouter(App);
